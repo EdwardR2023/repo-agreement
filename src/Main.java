cbe603bc611e1a5951feab4dd10110a064de4cb9
edward <edward_r04@yahoo.com>
2025-07-18 23:48:35 -0400

    Add DataLoader class and update Main to include it

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
-import models.Bond;
-import models.BorrowBond;
-import models.RepoDeal; 
 
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World! This is a Repo Agreement application.");
+        System.out.println("Hello, World! This is a Repo Agreement application. ");
     }
 }
---
+++ src/util/DataLoader.java
@@ -0,0 +1,8 @@
+package util;
+
+// This class is a placeholder for data loading functionality.
+
+
+public class DataLoader {
+    
+}
